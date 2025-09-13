684619d5e0a1dcf3bf7134fcf13061cbbf0445e2
Sujal Phaiju <sujalphaiju777@gmail.com>
2025-09-14 02:14:09 +0545

    terraform backend remote state added

---
+++ terraform-backend-bucket/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_bucket" {
+  bucket        = "nepse-terraform-state"
+  force_destroy = false
+
+  tags = {
+    Owner     = "Sujal Phaiju"
+    ManagedBy = "terraform"
+  }
+}
+
+# --- SSE Configuration ---
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_bucket_sse" {
+  bucket = aws_s3_bucket.tf_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# --- Versioning ---
+resource "aws_s3_bucket_versioning" "tf_bucket_versioning" {
+  bucket = aws_s3_bucket.tf_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,17 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
+terraform {
+  backend "s3" {
+    bucket         = "nepse-terraform-state"
+    key            = "nepse_project/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true
+  }
+}
+
+
 resource "random_id" "suffix" {
   byte_length = 4
 }
